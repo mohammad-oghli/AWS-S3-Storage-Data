f4119f5707691c146b7b6e21317ee60ad171f3df
mohammad-oghli <mhd.sh.oghli@gmail.com>
2024-06-21 00:09:10 +0400

    AWS S3 Storage Data Package v1.0

---
+++ README.md
@@ -0,0 +1,87 @@
+# AWS S3 Storage Data Package
+
+This Python package developed to streamline the interaction with Amazon Simple Storage Service S3 offered by AWS.
+
+It allows you to easily interact with data files on AWS S3 Storage using different operations such as Read, Write, Copy, Delete using boto3 API client integration with AWS.
+
+The functionality that offered by this package version:
+
+* Creating AWS S3 Storage object 
+* Read File from AWS S3 Storage
+* Write File to AWS S3 Storage
+* Copy File from AWS S3 Storage
+* Delete File from AWS S3 Storage
+* Copy Directory from AWS S3 Storage
+* Get File Permission
+* Set File Permission
+* Set Directory Permission
+
+This python package can be used to build data ingesting pipline for any AWS cloud application, complete data engineering pipeline, Model training pipeline and cloud native data solutions.
+
+## Installation Requirements 
+
+You need AWS IAM user with access key credentials in order to use this package
+
+You have 2 different methods to set up your AWS credentials
+
+### Using AWS Shared Credentials
+
+You need first to install [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) in your OS to set up your AWS shared credentials
+
+After installing AWS CLI for your OS open your terminal or command line and run the command:
+
+`aws configure`
+
+
+You will prompt to enter your AWS IAM user `AWS Access Key ID` and `AWS Secret Access Key`
+
+![](docs_assets/Screenshot from 2024-06-20 23-02-52.png)
+
+
+Shared credentials located by default in this file in your system `~/.aws/credentials`
+
+### Using Environment Variables
+
+In this method you don't need to install **AWS CLI**, but it's preferred to install it in your OS in case you need to check your aws services from you local machine
+
+You can set AWS credentials as environment variables in your OS 
+
+* On Linux and MacOS open your terminal and run the following commands
+
+    ``` 
+    export AWS_ACCESS_KEY_ID=your_access_key
+    export AWS_SECRET_ACCESS_KEY=your_secret_key
+   ```
+
+* On Windows open your command line and run the following commands
+    ``` 
+    set AWS_ACCESS_KEY_ID=your_access_key
+    set AWS_SECRET_ACCESS_KEY=your_secret_key
+   ```
+    
+## How to Use it
+
+* Copy `S3` module file into your project directory 
+* Install python package dependency 
+  * You can install the package either by adding it to your `requirement.txt` file:
+     
+    add `boto3==1.34.127` to it
+    
+    Then run `pip install -r requirements.txt`
+  * Or you can install it by just running:
+     
+    `pip install boto3==1.34.127`
+
+* Finally, you only need to import `StorageS3` class into your project source code:
+
+  `from S3 import StorageS3`
+
+## Documentation 
+
+This [Tutorial Notebook](tutorial_notebook.ipynb) contains comprehensive guide documentation on how to use AWS S3 Storage Data Package
+
+**Note:** `data_sample` directory contains all the data files used in AWS S3 tutorial notebook
+
+## Developer and Contributor
+
+**[Mohamad Oghli](https://github.com/mohammad-oghli)**
---
+++ S3.py
@@ -0,0 +1,108 @@
+import boto3
+
+
+class StorageS3:
+    recent_def_region = []
+
+    @classmethod
+    def set_default_region(cls, region_name):
+        if StorageS3.recent_def_region:
+            if StorageS3.recent_def_region[-1] != region_name:
+                StorageS3.recent_def_region.pop()
+                StorageS3.recent_def_region.append(region_name)
+                # Execute the code
+                boto3.setup_default_session(region_name=region_name)
+                print("New Default AWS Region has been set up")
+        else:
+            StorageS3.recent_def_region.append(region_name)
+            boto3.setup_default_session(region_name=region_name)
+            print("Default AWS Region has been set up")
+
+    def __init__(self, name, default_region='me-central-1'):
+        StorageS3.set_default_region(default_region)
+        # Default setting to use AWS configure shared credentials in your OS
+        # Credentials located by default in ~/.aws/credentials
+        # You can also set AWS credentials using OS environment variables
+        self.s3_client = boto3.client('s3')
+        self.name = name
+
+    def read_file(self, file_path):
+        response = self.s3_client.get_object(Bucket=self.name, Key=file_path)
+        object_content = response['Body'].read()
+        return object_content
+
+    def write_file(self, binary_data, output_path):
+        # if html:
+        #     self.s3_client.put_object(Body=binary_data, ContentType='text/html', Bucket=self.name, Key=output_path)
+        self.s3_client.put_object(Body=binary_data, Bucket=self.name, Key=output_path)
+
+    # def write_html_file(self, html_data, output_path):
+    #     self.s3_client.put_object(Body=html_data, ContentType='text/html', Bucket=self.name, Key=output_path)
+
+    def copy_file(self, src_file_path, dest_bucket, dest_file_path=''):
+        cp_src = {'Bucket': self.name, 'Key': src_file_path}
+        # Copy the object
+        self.s3_client.copy_object(
+            Bucket=dest_bucket,
+            Key=dest_file_path,
+            CopySource=cp_src
+        )
+        print(f"Copied {src_file_path} to {dest_file_path}")
+
+    def delete_file(self, file_path):
+        self.s3_client.delete_object(Bucket=self.name, Key=file_path)
+
+    def get_file_permission(self, file_path):
+        response = self.s3_client.get_object_acl(
+            Bucket=self.name,
+            Key=file_path,
+        )
+        return response['Grants']
+
+    def set_file_permission(self, file_path, public=False):
+        if public:
+            self.s3_client.put_object_acl(
+                ACL="public-read", Bucket=self.name, Key=file_path
+            )
+        else:
+            self.s3_client.put_object_acl(
+                ACL="private", Bucket=self.name, Key=file_path
+            )
+        return self.get_file_permission(file_path)
+
+    def copy_directory_files(self, src_dir_path, dest_bucket, dest_dir_path=''):
+        paginator = self.s3_client.get_paginator('list_objects_v2')
+        # Paginate through the source directory
+        for page in paginator.paginate(Bucket=self.name, Prefix=src_dir_path):
+            if 'Contents' in page:
+                for obj in page['Contents']:
+                    # Construct the source and destination object keys
+                    src_key = obj['Key']
+                    # Construct the destination key maintaining directory structure
+                    dest_key = dest_dir_path + src_key
+
+                    # Copy the object
+                    self.s3_client.copy_object(
+                        Bucket=dest_bucket,
+                        Key=dest_key,
+                        CopySource={'Bucket': self.name, 'Key': src_key}
+                    )
+                    print(f"Copied {src_key} to {dest_key}")
+
+    def set_directory_permission(self, dir_path, public=False):
+        paginator = self.s3_client.get_paginator('list_objects_v2')
+        acl_permission = "private"
+        if public:
+            acl_permission = "public-read"
+        # Paginate through the source directory
+        for page in paginator.paginate(Bucket=self.name, Prefix=dir_path):
+            if 'Contents' in page:
+                for obj in page['Contents']:
+                    # Construct the source object keys
+                    src_key = obj['Key']
+
+                    # Set permission for the object
+                    self.s3_client.put_object_acl(
+                        ACL=acl_permission, Bucket=self.name, Key=src_key
+                    )
+        print(f"Directory {dir_path} objects set to {acl_permission}")
